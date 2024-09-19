ccd6635deb6706877ae190b74c44239b22f9c8e8
Vishal Tomar <vishaltomar004@gmail.com>
2024-09-20 00:01:54 +0530

    First commit

---
+++ Solution/Step 13 Binary Tree/Lec 1/ntroductiontoTrees.java
@@ -0,0 +1,11 @@
+class Solution {
+    static int countNodes(int i) {
+        // code here
+        return (int)Math.pow(2,i-1);
+    }
+}class Solution {
+    static int countNodes(int i) {
+        // code here
+        return (int)Math.pow(2,i-1);
+    }
+}
